c09e7ea43adaaa0e3a927926b274438b77ead225
pch <panchanghe@fenxianglife.com>
2023-05-04 16:34:54 +0800

    优化代码、新增了example

--- aurora-examples/src/main/java/top/javap/aurora/example/api/UomgMapper.java
+++ aurora-examples/src/main/java/top/javap/aurora/example/api/UomgMapper.java
@@ -1,8 +1,11 @@
 package top.javap.aurora.example.api;
 
 import top.javap.aurora.annotation.Get;
+import top.javap.aurora.annotation.Header;
 import top.javap.aurora.annotation.Mapper;
 import top.javap.aurora.annotation.Param;
+import top.javap.aurora.annotation.Post;
+import top.javap.aurora.annotation.RequestBody;
 import top.javap.aurora.example.result.MusicResult;
 import top.javap.aurora.example.result.QingHuaResult;
 import top.javap.aurora.executor.AuroraFuture;
@@ -30,4 +33,11 @@ public interface UomgMapper {
 
     @Get("rand.music")
     MusicResult randMusic(@Param("sort") String sort, @Param("format") String format);
+
+    @Post("post")
+    MusicResult post(@RequestBody Object body,
+                     @Param("param1") String param1,
+                     @Param("param2") String param2,
+                     @Header("header1") String header1,
+                     @Header("header2") String header2);
 }
